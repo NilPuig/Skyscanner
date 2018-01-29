a07a24d583e81337fa73623d12c16205fd834c2c
nil <nil.puigm@gmail.com>
2018-01-29 21:04:10 +0000

    Initial Commit

---
+++ SkyScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkyScanner
+//
+//  Created by Nil Puig on 29/1/18.
+//  Copyright © 2018 Nil Puig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SkyScannerTests/SkyScannerTests.swift
@@ -0,0 +1,36 @@
+//
+//  SkyScannerTests.swift
+//  SkyScannerTests
+//
+//  Created by Nil Puig on 29/1/18.
+//  Copyright © 2018 Nil Puig. All rights reserved.
+//
+
+import XCTest
+@testable import SkyScanner
+
+class SkyScannerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
